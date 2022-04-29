d5093591556c0b27bfc4849740ed7e92d106db78
tega <kesienametitiri024@gmail.com>
2022-04-29 08:17:27 +0100

    Add Address costructor and methods in my scripts for my Address-Book

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,13 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+function Address(loc, street, city, state) {
+  this.loc = loc;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.loc + ": " + this.street + ", " + this.city + ", " + this.state;
+}
